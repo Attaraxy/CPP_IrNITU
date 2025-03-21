f3046225538cb79d4ddb6e8b31b6906e33534384
Attaraxy <maxgal1303@gmail.com>
2025-03-21 19:32:50 +0800

    Первое задание

---
+++ Task1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a {15};
+    int b {6};
+    int c {a + b};
+    std::cout << "c = " << c << std::endl;
+    int c1 {a - b};
+    std::cout << "c1 = " << c1 << std::endl;
+
+
+    float a2 {24};
+    float b2 {5};
+    float c2 {a2 / b2};
+    double d {a2 / b2};
+    double {4 / b2};
+    std::cout << "c2 = " << c2 << std::endl;
+    std::cout << "d = " << d << std::endl;
+
+        return 0;
+}
